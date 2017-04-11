3cd2fc86fb32e7ffbe99831aeb28b4fbf7e5a66c
pq <pq.blogspot@gmail.com>
2017-04-11 17:40:16 +0900

    First Commit

---
+++ ByteToImage/src/imagetobyte.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import binascii
+import os
+import sys
+import imghdr
+import glob
+def main():
+    fd = "images"
+    if not os.path.exists(fd):
+        os.mkdir(fd)
+        print("Please put the image file into the image folder you want to convert to the hexadecimal string.")
+        sys.exit()
+    os.chdir(fd)  
+    for imagefile in glob.iglob("*.*"):
+        imagetype = imghdr.what(imagefile)
+        if imagetype:
+            with open(imagefile,"rb") as fp:
+                hexdata = binascii.b2a_hex(fp.read()) 
+                print("\n" + fp.name)
+                print(hexdata)
+if __name__ == "__main__":
+    sys.exit(main())
